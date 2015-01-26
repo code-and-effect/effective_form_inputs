322a56f0a53aeea8324d2c87977d0d61ae667c0e
Matt Riemer <matthew@agilestyle.com>
2015-01-26 12:02:58 -0700

    Refactored into base class

--- app/models/inputs/effective_date_time_picker/field.rb
+++ app/models/inputs/effective_date_time_picker/field.rb
@@ -1,18 +1,14 @@
 module Inputs
   module EffectiveDateTimePicker
-    class Field
+    class Field < Effective::FormField
       delegate :content_tag, :text_field_tag, :to => :@template
 
-      def initialize(object, object_name, template, method, opts)
-        @object = object
-        @object_name = object_name
-        @template = template
-        @method = method
-        @opts = opts
+      def default_input_js_options
+        {:format => 'YYYY-MM-DD h:mm A', :sideBySide => true}
       end
 
-      def default_js_options
-        {:format => 'YYYY-MM-DD h:mm A', :sideBySide => true}
+      def default_input_classes
+        [:effective_date_time_picker, :datetime]
       end
 
       def to_html
@@ -26,47 +22,15 @@ module Inputs
 
       private
 
-      # These two are for the text field
-
-      def field_name
-        @object_name + "[#{@method}]"
-      end
-
-      def value
-        @object.send(@method)
-      end
-
       def options
-        @field_options ||= (@opts || {}).tap do |options|
-
-          # Add appropriate classes
-          [:effective_date_time_picker, :datetime].each do |c|
-            if options[:class].blank?
-              options[:class] = c.to_s
-            elsif options[:class].kind_of?(Array)
-              options[:class] << c unless options[:class].include?(c)
-            elsif options[:class].kind_of?(String)
-              options[:class] << (' ' + c.to_s) unless options[:class].include?(c.to_s)
-            end
-          end
+        (@opts || {}).tap do |options|
 
           unless (options['data-input-js-options'][:format].present? rescue false)
             options[:pattern] = '\d{4}-\d{2}-\d{2} \d+:\d{2} [A-Z]{2}'
           end
 
-          merged_input_js_options(options)
-        end
-      end
-
-      def merged_class_options(*classes)
-
-      end
-
-      def merged_input_js_options(opts)
-        (opts || {}).tap do |options|
-          options['data-input-js-options'] = (
-            JSON.generate((default_js_options || {}).merge(options['data-input-js-options'] || {})) rescue {}
-          )
+          merge_class_options!(options)
+          merge_input_js_options!(options)
         end
       end
 
