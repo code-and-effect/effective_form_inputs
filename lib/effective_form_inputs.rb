d7f441e13ceafc1200170ff432a8888905558c3f
Matt Riemer <matthew@agilestyle.com>
2015-01-27 10:12:09 -0700

    Add required to reject Js options

--- lib/effective_form_inputs.rb
+++ lib/effective_form_inputs.rb
@@ -3,5 +3,5 @@ require "effective_form_inputs/engine"
 require "effective_form_inputs/version"
 
 module EffectiveFormInputs
-  REJECTED_INPUT_JS_OPTIONS = [:as, :hint, :error, :readonly, :disabled, :label, :inline_label, :placeholder, :prompt, :min_max, :maxlength, :pattern, :input_html, :wrapper_html, :error_html, :id, :wrapper, :collection]
+  REJECTED_INPUT_JS_OPTIONS = [:as, :hint, :error, :readonly, :disabled, :required, :label, :inline_label, :placeholder, :prompt, :min_max, :maxlength, :pattern, :input_html, :wrapper_html, :error_html, :id, :wrapper, :collection]
 end
