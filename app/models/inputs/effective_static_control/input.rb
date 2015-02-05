3e516dc5fe808215b56e695469090795abb24344
Matt Riemer <matthew@agilestyle.com>
2015-02-05 13:53:06 -0700

    remove form-control class from static_control input

--- app/models/inputs/effective_static_control/input.rb
+++ app/models/inputs/effective_static_control/input.rb
@@ -3,8 +3,18 @@ module Inputs
     class Input < Effective::FormInput
       delegate :content_tag, :to => :@template
 
+      def default_input_classes
+        ['form-control-static']
+      end
+
       def to_html
-        content_tag(:p, value, :class => 'form-control-static')
+        if options[:class].kind_of?(Array)
+          options[:class].delete('form-control')
+        elsif options[:class].kind_of?(String)
+          options[:class].gsub!('form-control', '')
+        end
+
+        content_tag(:p, value, options)
       end
 
     end
