06efa370091d951a6fd5a86a52c7d73cee2ff5e6
Matt Riemer <matthew@agilestyle.com>
2015-06-04 00:59:35 -0600

    remove unneeded code

--- app/models/effective/form_builder_inputs.rb
+++ app/models/effective/form_builder_inputs.rb
@@ -18,15 +18,5 @@ module Effective
       options[:value_method] = :to_s if options[:value_method].nil?
       Inputs::EffectiveSelect::Input.new(@object, @object_name, @template, method, options, html_options).to_html
     end
-
-    # private
-
-    # def merged_input_js_options(options)
-    #   (options || {}).tap do |opts|
-    #     js_options = opts.reject { |k, _| EffectiveFormInputs::REJECTED_INPUT_JS_OPTIONS.include?(k) }
-    #     opts.reject! { |k, _| js_options.include?(k) }
-    #     opts['data-input-js-options'] = js_options if js_options.present?
-    #   end
-    # end
   end
 end
