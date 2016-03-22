c36a77b905cf01dbc0e6d448c54e46e90fbbd0cf
Matt Riemer <matthew@agilestyle.com>
2016-03-22 10:43:47 -0600

    add bootstrap contentsCss to ckeditor_text_area

--- app/models/inputs/effective_ckeditor_text_area/input.rb
+++ app/models/inputs/effective_ckeditor_text_area/input.rb
@@ -8,6 +8,7 @@ module Inputs
           effective_assets: defined?(EffectiveAssets).present?,
           effective_ckeditor_js_path: asset_path('effective_ckeditor.js'),
           effective_ckeditor_css_path: asset_path('effective_ckeditor.css'),
+          contentsCss: (options[:contentCss] || "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"),
           toolbar: options[:toolbar],
           height: options[:height],
           width: options[:width]
