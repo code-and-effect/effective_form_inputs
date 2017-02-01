aa7b22d080e5419c69735d61f787b15f8af922e0
Matt Riemer <matthew@agilestyle.com>
2017-02-01 13:39:25 -0700

    Better html_safe call. Weird bug.

--- app/models/inputs/effective_static_control/input.rb
+++ app/models/inputs/effective_static_control/input.rb
@@ -11,7 +11,7 @@ module Inputs
         if value.kind_of?(String) && value.start_with?('<p>') && value.end_with?('</p>')
           content_tag(:p, value[3...-4].html_safe, tag_options)
         else
-          content_tag(:p, (value.html_safe? ? value : value.to_s.html_safe), tag_options)
+          content_tag(:p, (value.html_safe? ? value : (value.to_s || '').html_safe), tag_options)
         end
 
       end
