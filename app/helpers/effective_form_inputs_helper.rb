20ff60be7ec444decd3ef51d16dac2130a4cb51f
Matt Riemer <matthew@codeandeffect.com>
2018-09-08 11:06:30 -0600

    simple_form_save

--- app/helpers/effective_form_inputs_helper.rb
+++ app/helpers/effective_form_inputs_helper.rb
@@ -4,4 +4,14 @@ module EffectiveFormInputsHelper
     raise 'price_to_currency expects an Integer representing the number of cents' unless price.kind_of?(Integer)
     number_to_currency(price / 100.0)
   end
+
+  def simple_form_save(form, label = 'Save', options = {}, &block)
+    wrapper_options = { class: 'form-actions' }.merge(options.delete(:wrapper_html) || {})
+    options = { class: 'btn btn-primary', data: { disable_with: 'Saving...'} }.merge(options)
+
+    content_tag(:div, wrapper_options) do
+      form.button(:submit, label, options) + (capture(&block) if block_given?)
+    end
+  end
+
 end
