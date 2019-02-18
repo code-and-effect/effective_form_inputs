b884f46c6c395b1e738f91a04d38b781db5d88cb
Matt Riemer <matthew@codeandeffect.com>
2019-02-18 13:49:33 -0700

    work with ActsAsArchived automatically

--- app/models/inputs/effective_select_input.rb
+++ app/models/inputs/effective_select_input.rb
@@ -13,6 +13,26 @@ if defined?(SimpleForm)
 
       Inputs::EffectiveSelect::Input.new(object, object_name, template, attribute_name, input_options, (merge_wrapper_options(input_html_options, wrapper_options) || {})).to_html
     end
+
+    # Apply ActsAsArchived behavior.
+    def collection
+      @collection ||= begin
+        collection = translate(options.delete(:collection)) || self.class.boolean_collection
+        collection.respond_to?(:call) ? collection.call : collection.to_a
+      end
+    end
+
+    def translate(collection)
+      return collection unless object.respond_to?(:new_record?)
+      return collection unless collection.respond_to?(:klass) && collection.klass.respond_to?(:acts_as_archived?)
+
+      if object.new_record?
+        collection.unarchived
+      else
+        collection.unarchived.or(collection.archived.where(collection.klass.primary_key => object.send(attribute_name)))
+      end
+    end
+
   end
 
 end
